9d481d3a4e6cd06a9ffa70d540208c98e14da4b2
Domen Gabrovsek <domen.gabrovsek@gmail.com>
2019-05-19 19:51:28 +0200

    Rewrote stellar functions to use request-promise. Introduced basic error handling. Code restructure

--- src/utils/file-utils.js
+++ src/utils/file-utils.js
@@ -2,22 +2,26 @@
 
 const fs = require('fs');
 
+// write data to file
 const writeToFile = (file, data) => {
     fs.writeFileSync(file, JSON.stringify(data), err => {
         if(err) return console.log(err);
 
-        console.log(`Data written to ${fullPath}`);
+        return fullPath;
     });
 };
 
 // read data from file and return it
-const readFromFile = file => JSON.parse(fs.readFileSync(file).toString());
+const readFromFile = (file) => {
+    let result = {};
 
-// round number 
-const round = (input, places) => Number.parseFloat(input).toFixed(places);
+    try { result.data = JSON.parse(fs.readFileSync(file).toString()); } 
+    catch (err){ result.error = err; }
+
+    return result;
+};
 
 module.exports = {
     writeToFile,
-    readFromFile,
-    round
+    readFromFile
 }
--- test.js
+++ test.js
@@ -1,68 +1,34 @@
-const { getAccountInfo, getPaymentsForAccount } = require('./src/Stellar/stellar');
-const { getAssetPairs, getTickerInfo } = require('./src/Kraken/kraken');
-const { stellar, kraken } = require('./files/keys.json');
-const { writeToFile, readFromFile } = require('./src/utils');
-const path = require('path');
 
-const pair = 'XXLMZEUR';
+const { getAssetPairs, getTickerInfo } = require('./src/api-calls/kraken');
+const { getPaymentsForAccount } = require('./src/api-calls/stellar');
+const { stellar } = require('./files/keys.json');
+const { getAccountInfo } = require('./src/utils/utils');
 
-const filesPath = path.join(__dirname, '/files');
+getAccountInfo(stellar.accountId)
+    .then((response, error) => {
+        if(error){
+            return console.dir(error);
+        }
 
-const payments = readFromFile(`${filesPath}/payments.json`);
-const accountInfo = readFromFile(`${filesPath}/accountInfo.json`);
-
-
-getAccountInfo(stellar.accountId, ({ body }) => {
-    const data = {
-        accountId: body.account_id,
-        inflationDestination: body.inflation_destination,
-        balanceXLM: body.balances[0].balance
-    };
-
-    getTickerInfo(pair, (error, response) => {
-        if(error) { return console.dir(error); }
-
-        const prices = response.body.result[pair];
-        const ask = prices.a[0];
-
-        data.balanceEUR =  data.balanceXLM * ask;
-
-        console.dir(data);
+        return console.dir(response);
     });
 
+// max is 200
+const limit = 200;
+const order = 'desc';
 
+getPaymentsForAccount(stellar.accountId, { limit, order })
+    .then((response, error) => {
+        if(error){
+            return console.dir(error);
+        }
 
-});
+        return console.dir(response);
+    });
 
-//     writeToFile(path.join(__dirname, '/files/accountInfo.json'), data);
-// });
-
-// const limit = 100;
-// const order = 'desc';
-
-// getPaymentsForAccount(stellarAccount, { limit, order }, (error, response) => {
-//     if(error){
-//         return console.dir(error);
+// const pair = 'XXLMZEUR';
+// const tickerInfo = getTickerInfo(pair).then((response, error) => {
+//     if(error) {
+//         return console.log(error);
 //     }
-
-//     const { records } = response.body._embedded;
-
-//     const data = records.map(record => {
-//         return {
-//             transactionId: record.transaction_hash,
-//             successful: record.transaction_successful,
-//             source: record.from,
-//             destination: record.to,
-//             amount: record.amount
-//         };
-//     });
-
-//     writeToFile('payments', data);
-// });
-
-// getAssetPairs((error, response) => {
-//     if(error) { return console.dir(error); }
-
-//     const assetPairs = Object.keys(response.body.result);
-//     return console.dir(assetPairs);
 // });
